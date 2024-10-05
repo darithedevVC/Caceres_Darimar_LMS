b1997149d865c42c390851c88dfd66b188660e8f
Dari <darithedev@gmail.com>
2024-10-05 19:07:20 -0400

    Update comments in Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Objects;
@@ -12,12 +11,14 @@ import java.util.List;
     Software Development I - CEN 3024C - 14320
     09.08.24
 
-    The class name is 'Main' and the function of this class is to prompt the user with 3 choices in an endless
-    while loop until the user chooses '0' to exit. The three choices corresponds to maintaining and accessing
-    this simple Library Management Software.
+    The class name is 'Main' and the function of this class is to prompt the user 
+    with 3 choices in an endless while loop until the user chooses '0' to exit. 
+    The three choices corresponds to maintaining and accessing this simple Library 
+    Management Software.
 
-    The overall program objective is to manage a simple console based library management system where the end
-    user(s) can add, remove, and see their collection of books by importing the collection of books in txt files.
+    The overall program objective is to manage a simple console based library 
+    management system where the end user(s) can add, remove, and see their 
+    collection of books by importing the collection of books in txt files.
 */
 
 public class Main {
