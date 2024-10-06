fd95d01ad7bbf37991d8c9ff53cb6726385857a9
Dari <darithedev@gmail.com>
2024-10-06 18:08:44 -0400

    Updated menu and switch in Main.java

--- Main.java
+++ Main.java
@@ -5,14 +5,18 @@ import java.util.Objects;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
+//
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOExceptiom;
 
 /*
     Darimar Caceres
     Software Development I - CEN 3024C - 14320
     09.08.24
 
-    The class name is 'Main' and the function of this class is to prompt the user 
     with 3 choices in an endless while loop until the user chooses '0' to exit. 
+    The class name is 'Main' and the function of this class is to prompt the user 
     The three choices corresponds to maintaining and accessing this simple Library 
     Management Software.
 
@@ -25,34 +29,49 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Library library = new Library();
+        Scanner file = new Scanner (System.in);
+
         System.out.print("Hello and welcome!\nWhat would you like to do: ");
 
+        /* PLAN TO MOVE THESE TO LIBRARY
+        
         boolean exit = false;
         int count = 0;
 
         List<String> books = new ArrayList<String>();
-
+        */
         while (!exit) {
 
             System.out.print("\n***(To select one please enter the corresponding number i.e. 1,2,3 etc)***\n\n");
-            System.out.print(   "1. Add new books to the collection.\n" +
-                                "2. Remove book from the collection.\n" +
-                                "3. List all books currently in the collection\n" +
+            System.out.print(   "1. Add new books to the collection using file name.\n" +
+                                "2. List all books currently in the collection\n" +
+                                "   Remove book from the collection using:\n" +
+                                "   3. BARCODE (book removal)\n" +
+                                "   4. TITLE NUMBER (book removal)\n" +
+                                "5. Check-out book\n" +
+                                "6. Check-in book\n" +
+                                
                                 "Enter '0' to exit.\n\n" +
                                 "Option: ");
 
-            Scanner input = new Scanner(System.in);
+            Scanner option = new Scanner(System.in);
 
             try {
-                int choice = input.nextInt();
+                int choice = option.nextInt();
 
                 // switch case that corresponds to end user choice
                 switch (choice) {
-                    // case 1 will add books to the library collection
+                    // case 1 will ask user for file name and add books to the library collection
                     case 1:
-                        System.out.print("Adding books to the collection...\n");
+                        System.out.print("What is the file name of books you are adding to the collection?\n");
+                        
+                        
 
-                        Scanner file = new Scanner (new File("/Users/DariTheDev/IdeaProjects/LMS/src/books.txt"));
+                        /* THIS ALL WILL BE MOVED TO LIBRARY
+                        new File("/Users/DariTheDev/IdeaProjects/LMS/src/books.txt")
+                        //THIS IS SHORT MESSAGE THAT PRINTING IS TAKING PLACE
+                        System.out.print("Adding books to the collection...\n");
 
                         while (file.hasNextLine()) {
 
@@ -60,26 +79,29 @@ public class Main {
                             books.add(count, book);
                             System.out.printf("Added: " + books.get(count) + "\n");
                             count++;
-                        }
+                        }*/
                         break;
-                    // case 2 will remove books from the library collection
-                    case 2:
+                    // case 2 will list books from the library collection "database"
+                    case 2: 
+                    /*  MOVE ALL THIS TO LIBRARY
                         System.out.print("Enter the id of the book you want deleted.\n");
 
-                        int id_choice = input.nextInt();
+                        int id_choice = option.nextInt();
 
                         // iterates through the library collection of books
-                        // book is removed if book index matches users input choice
+                        // book is removed if book index matches users option choice
                         for (int i = 0; i < books.size(); i++) {
                             int num = id_choice - 1;
                              if (Objects.equals(books.get(num), books.get(i))) {
                                  System.out.printf(books.get(num) + " was removed.\n");
                                  books.remove(num);
                              }
-                        }
+                        }*/
                         break;
-                    // case 3 will list the books currently in the library collection
-                    case 3:
+                    //case 3 will remove books using barcode number from the library collection
+                    case 3: 
+                    /* MOVE ALL THIS TO LIBRARY CLASS
+                    //THIS CASE REMOVES BOOK BASE ON BARCODE NUMBER
                         System.out.print("Books currently in your collection: \n");
 
                         int counter = 0;
@@ -88,7 +110,19 @@ public class Main {
 
                             System.out.printf(books.get(counter) + "\n");
                             counter++;
-                        }
+                        }*/
+                        break;
+                    // case 4 will remove books using title number from the library collection
+                    case 4: //THIS CASE REMOVES BOOK BASE ON TITLE NUMBER
+
+                        break;
+                    // case 5 will check-out books using ? number from the library collection
+                    case 5: 
+
+                        break;
+                    // case 6 will check-in books using ? number from the library collection
+                    case 6: 
+
                         break;
                     // exits program
                     case 0:
