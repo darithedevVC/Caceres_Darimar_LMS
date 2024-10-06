b3eb06558f97a778a9af2ef1c7fdbe184c96af2a
Dari <darithedev@gmail.com>
2024-10-06 19:17:16 -0400

    Update comments for Book.java, Update switch case 1 adding books in Main.java

--- Book.java
+++ Book.java
@@ -1,1 +1,13 @@
-/* Creates book objects */
+/* 
+    Darimar Caceres
+    Software Development I - CEN 3024C - 14320
+    10.06.24
+
+    The class name is 'Book' and the function of this class is to create book objects
+    using ... COMPLETE THISSSS
+
+    The overall program objective is to manage a simple console based library 
+    management system where the end user(s) can add, remove, and see their 
+    collection of books by importing the collection of books in txt files.
+
+*/
--- Main.java
+++ Main.java
@@ -13,10 +13,10 @@ import java.io.IOException;
 /*
     Darimar Caceres
     Software Development I - CEN 3024C - 14320
-    09.08.24
+    10.06.24
 
-    with 3 choices in an endless while loop until the user chooses '0' to exit. 
-    The class name is 'Main' and the function of this class is to prompt the user 
+    The class name is 'Main' and the function of this class is to prompt the user
+    with 3 choices in an endless while loop until the user chooses '0' to exit.  
     The three choices corresponds to maintaining and accessing this simple Library 
     Management Software.
 
@@ -29,8 +29,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Library library = new Library();
-        Scanner file = new Scanner (System.in);
+        Library library = new Library();
+        Scanner scanFile = new Scanner (System.in);
 
         System.out.print("\nHello and welcome!\n\nWhat would you like to do: \n1");
 
@@ -43,14 +43,15 @@ public class Main {
 
         List<String> books = new ArrayList<String>();
         */
+
         while (!exit) {
 
             System.out.print("\n***(To select one please enter the corresponding number i.e. 1,2,3 etc)***\n\n");
-            System.out.print(   "1. Add new books to the collection using file name.\n" +
+            System.out.print(   "1. Add new books to the collection using file name.\n" + // could this erase existing books / how to handle that 
                                 "2. List all books currently in the collection\n" +
                                 "   Remove book from the collection using:\n" +
                                 "   3. BARCODE (book removal)\n" +
-                                "   4. TITLE NUMBER (book removal)\n" +
+                                "   4. TITLE (book removal)\n" +
                                 "5. Check-out book\n" +
                                 "6. Check-in book\n" +
                                 
@@ -66,10 +67,39 @@ public class Main {
                 switch (choice) {
                     // case 1 will ask user for file name and add books to the library collection
                     case 1:
+
                         System.out.print("What is the file name of books you are adding to the collection?\n");
                         
-                        
+                        String file = scanFile.nextline();
 
+                        try (BufferedReader b = new BufferedReader(new FileReader(file))) {
+                            
+                            String line;
+
+                            while ((line = b.readline()) != null) {
+
+                                String[] book = line.split(",");
+
+                                // If statement checks for 3 elements (Book Barcode, Title, and Author) per line
+                                if (book.length == 3) {
+
+                                    String barcodeID = book[0];
+                                    String title = book[1];
+                                    String author = book[2];
+
+                                    library.addBook(new Book(barcodeID, title, author));
+
+                                } else {
+
+                                    System.out.println("File has invalid book format (barcodeID, Book title, Book author). Try again!\n");
+                                }
+                            }
+
+                        } catch (IOException e) {
+                            
+                            System.out.println("Error! Error message: " + e.getMessage() + "\n");
+                        }
+                        
                         /* THIS ALL WILL BE MOVED TO LIBRARY
                         new File("/Users/DariTheDev/IdeaProjects/LMS/src/books.txt")
                         //THIS IS SHORT MESSAGE THAT PRINTING IS TAKING PLACE
@@ -114,8 +144,8 @@ public class Main {
                             counter++;
                         }*/
                         break;
-                    // case 4 will remove books using title number from the library collection
-                    case 4: //THIS CASE REMOVES BOOK BASE ON TITLE NUMBER
+                    // case 4 will remove books using the title from the library collection
+                    case 4: //THIS CASE REMOVES BOOK BASE ON TITLE
 
                         break;
                     // case 5 will check-out books using ? number from the library collection
