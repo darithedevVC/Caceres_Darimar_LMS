cabac5f32d18fd098e815823e68a2cff1c41a4fc
Dari <darithedev@gmail.com>
2024-10-06 23:57:04 -0400

    update comments for Main.java

--- Main.java
+++ Main.java
@@ -8,10 +8,9 @@ import java.io.IOException;
     Software Development I - CEN 3024C - 14320
     10.06.24
 
-    The class name is 'Main' and the function of this class is to prompt the user
-    with 3 choices in an endless while loop until the user chooses '0' to exit.  
-    The three choices corresponds to maintaining and accessing this simple Library 
-    Management Software.
+    The function of the 'Main' class is to prompt the user with six choices
+    in an endless while loop until the user chooses '0' to exit. The six choices
+    corresponds to maintaining and accessing this simple Library Management Software.
 
     The overall program objective is to manage a simple console based library 
     management system where the end user(s) can add, remove, and see their 
